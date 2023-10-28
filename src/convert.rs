108e3a5ce35db0ac3260ab38f7800372a19a2cdc
darcy <acednes@gmail.com>
2023-10-28 22:15:18 +1100

    use single `resize` call to scale image

--- src/convert.rs
+++ src/convert.rs
@@ -13,14 +13,8 @@ const MIN_ALPHA: u8 = 200;
 /// Convert an image into an ASCII string
 pub fn image_to_ascii(image: DynamicImage, color: bool) -> String {
     // Fit image to terminal size
-    // Height must be doubled, because it is halved below
     let (max_width, max_height) = term_size::dimensions().expect("Failed to get terminal size");
-    let image = image.resize(max_width as u32, max_height as u32 * 2, FilterType::Nearest);
-
-    // Shrink image height by half
-    // Accounts for 1:2 character ratio
-    let (image_width, image_height) = image.dimensions();
-    let image = image.resize_exact(image_width, image_height / 2, FilterType::Nearest);
+    let image = resize_image_to_fit(&image, max_width as u32, max_height as u32);
 
     // Loop every pixel in image
     let (image_width, image_height) = image.dimensions();
@@ -63,4 +57,30 @@ pub fn image_to_ascii(image: DynamicImage, color: bool) -> String {
     }
 
     output
+}
+
+/// Resize an image to fit maximum width and height bounds
+///
+/// This was copied from `image.resize`
+fn resize_image_to_fit(image: &DynamicImage, max_width: u32, max_height: u32) -> DynamicImage {
+    let (width, height) = image.dimensions();
+
+    // Scale width by 2, to account for 1:2 character ratio in terminal
+    // If this wasn't here, `image.resize` could have been used instead
+    // This could have also been done in 2 passes (`resize_exact` and `resize`), but this would be
+    // slightly slower
+    let width = 2 * width;
+
+    // Minimum of ratios between original and max (width or height)
+    let ratio = f32::min(
+        max_width as f32 / width as f32,
+        max_height as f32 / height as f32,
+    );
+
+    // Scale based on minimum ratio
+    let new_width = (width as f32 * ratio).round().max(1.0) as u32;
+    let new_height = (height as f32 * ratio).round().max(1.0) as u32;
+
+    // Resize image, assuming aspect ratio is already preserved
+    image.resize_exact(new_width as u32, new_height as u32, FilterType::Nearest)
 }
